5e2e96411e7a6930babeb5c67f50f63e1cc21cd5
Tatsuya Maki <t28oogle@gmail.com>
2020-07-24 23:28:02 +0900

    Add AbstractSlideShareOperations to commonize duplicated implementaions of operations (#10)

---
+++ spring-social-slideshare/src/main/kotlin/io/t28/springframework/social/slideshare/api/impl/AbstractSlideShareOperations.kt
@@ -0,0 +1,42 @@
+package io.t28.springframework.social.slideshare.api.impl
+
+import org.springframework.social.MissingAuthorizationException
+import org.springframework.web.util.UriComponentsBuilder
+
+/**
+ * Abstract superclass for implementation of SlideShare operations
+ *
+ * @constructor
+ * @param isAuthorized Whether or not it is authorized.
+ */
+abstract class AbstractSlideShareOperations(private val isAuthorized: Boolean) {
+    /**
+     * Throws a [MissingAuthorizationException] if it is not authorized.
+     *
+     * @throws MissingAuthorizationException If it is not authorized.
+     */
+    @Throws(MissingAuthorizationException::class)
+    protected fun requireAuthorization() {
+        if (isAuthorized) {
+            return
+        }
+        throw MissingAuthorizationException(PROVIDER_ID)
+    }
+
+    companion object {
+        const val PROVIDER_ID = "SlideShare"
+
+        private const val API_BASE_URL = "https://www.slideshare.net/api/2/"
+
+        /**
+         * Build a SlideShare API URI string.
+         *
+         * @param path The path of operation.
+         * @param init Initializer.
+         */
+        @JvmStatic
+        protected fun buildUriString(path: String, init: UriComponentsBuilder.() -> Unit = {}): String {
+            return UriComponentsBuilder.fromUriString(API_BASE_URL).path(path).apply(init).toUriString()
+        }
+    }
+}
